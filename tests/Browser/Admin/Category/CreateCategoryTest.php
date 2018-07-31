c64acb2e292a91840a060b46f95e60f87977d506
at-namtran <nam.tran@asiantech.vn>
2018-07-31 10:18:19 +0700

    test create category

---
+++ tests/Browser/Admin/Category/CreateCategoryTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Browser\Admin\Category;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\Browser\Pages\Admin\Category\CreateCategoryPage;
+use App\Models\Category;
+
+class CreateCategoryTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    public function setUp()
+    {
+        parent::setUp();
+        factory(Category::class, 5)->create();
+    }
+
+    /**
+     * A Dusk test it can add new category
+     *
+     * @return void
+     */
+    public function test_it_can_add_new_caterory()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(new CreateCategoryPage)
+                    ->type('name', 'Phim Hành Động')
+                    ->press('Submit')
+                    ->assertPathIs('/admin/categories');
+            $this->assertDatabaseHas('categories', ['name' => 'Phim Hành Động']);
+        });
+    }
+
+    /**
+     * A Dusk test it can add fail category
+     *
+     * @return void
+     */
+    public function test_it_can_add_new_caterory_fail()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(new CreateCategoryPage)
+                    ->type('name', '')
+                    ->press('Submit')
+                    ->assertPathIs('/admin/categories/create')
+                    ->assertSee('Please enter name');
+        });
+    }
+}
---
+++ tests/Browser/Pages/Admin/Category/CreateCategoryPage.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Browser\Pages\Admin\Category;
+
+use Laravel\Dusk\Browser;
+use Laravel\Dusk\Page;
+
+class CreateCategoryPage extends Page
+{
+    /**
+     * Get the URL for the page.
+     *
+     * @return string
+     */
+    public function url()
+    {
+        return '/admin/categories/create';
+    }
+
+    /**
+     * Assert that the browser is on the page.
+     *
+     * @param  Browser  $browser
+     * @return void
+     */
+    public function assert(Browser $browser)
+    {
+        $browser->assertPathIs($this->url())
+                ->assertTitleContains('Add Category')
+                ->assertSee('Add Category')
+                ->assertSee('Name');
+    }
+
+    /**
+     * Get the element shortcuts for the page.
+     *
+     * @return array
+     */
+    public function elements()
+    {
+        return [
+            '@element' => '#selector',
+        ];
+    }
+}
