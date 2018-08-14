0b530fe27ba310f3d6d21aceb56b8a46b7816627
at-khanhnguyen <khanh.nguyen@asiantech.vn>
2018-08-14 17:29:40 +0700

    api login logout for user

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['as' => 'api.','namespace' => 'Api\User'], function () {
+    Route::post('/login', 'LoginController@login');
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::post('/logout', 'LoginController@logout');
+    });
+});
