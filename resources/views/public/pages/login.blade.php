d7cd6767b498eee3ab1f2ca1317fc31207fb37b3
at-namtran <nam.tran@asiantech.vn>
2018-08-15 09:47:13 +0700

    fix hash code

--- resources/views/public/pages/login.blade.php
+++ resources/views/public/pages/login.blade.php
@@ -36,9 +36,9 @@
             <div class="contact_info">
                <h2>@lang('user/login.find_us_here')</h2>
                <div class="map">
-                  <iframe width="100%" height="175" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.co.in/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Lighthouse+Point,+FL,+United+States&amp;aq=4&amp;oq=light&amp;sll=26.275636,-80.087265&amp;sspn=0.04941,0.104628&amp;ie=UTF8&amp;hq=&amp;hnear=Lighthouse+Point,+Broward,+Florida,+United+States&amp;t=m&amp;z=14&amp;ll=26.275636,-80.087265&amp;output=embed">
+                  <iframe width="100%" height="175" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="@lang('user/map.src')">
                     </iframe><br><small>
-                    <a href="https://maps.google.co.in/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Lighthouse+Point,+FL,+United+States&amp;aq=4&amp;oq=light&amp;sll=26.275636,-80.087265&amp;sspn=0.04941,0.104628&amp;ie=UTF8&amp;hq=&amp;hnear=Lighthouse+Point,+Broward,+Florida,+United+States&amp;t=m&amp;z=14&amp;ll=26.275636,-80.087265" style="color:#888;text-align:left;font-size:0.85em">@lang('user/login.form.view')</a></small>
+                    <a href="@lang('user/map.href')" style="color:#888;text-align:left;font-size:0.85em">@lang('user/login.form.view')</a></small>
                </div>
             </div>
             <div class="company_address">
@@ -46,10 +46,10 @@
                <p>@lang('user/login.address'),</p>
                <p>@lang('user/login.street'),</p>
                <p>@lang('user/login.country')</p>
-               <p>@lang('user/login.phone'):(00) 222 666 444</p>
-               <p>@lang('user/login.fax'): (000) 000 00 00 0</p>
+               <p>@lang('user/login.phone'): @lang('user/login.phone_number')</p>
+               <p>@lang('user/login.fax'): @lang('user/login.fax_number')</p>
                <p>@lang('user/login.email'): <span><a href="#">info(at)mycompany.com</a></span></p>
-               <p>@lang('user/login.follow'): <span><a href="#">Facebook</a></span>, <span><a href="#">Twitter</a></span></p>
+               <p>@lang('user/login.follow'): <span><a href="#">@lang('user/login.facebook')</a></span>, <span><a href="#">@lang('user/login.twitter')</a></span></p>
             </div>
          </div>
       </div>
