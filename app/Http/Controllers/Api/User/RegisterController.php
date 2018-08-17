9978a19d10fbe5c1bb257ff40e9c318ee296d81a
at-khanhnguyen <khanh.nguyen@asiantech.vn>
2018-08-17 11:00:15 +0700

    create template for job send mail

--- app/Http/Controllers/Api/User/RegisterController.php
+++ app/Http/Controllers/Api/User/RegisterController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\User\RegisterUserRequest;
 use App\Models\User;
 use App\Http\Controllers\Api\ApiController;
+use App\Jobs\SendMailJob;
 
 class RegisterController extends ApiController
 {
@@ -21,14 +22,21 @@ class RegisterController extends ApiController
     public function register(RegisterUserRequest $request)
     {
         $user = $request->all();
+        $datas = [
+            'email' => $user['email'],
+            'password' => $user['password']
+        ];
         $user['password'] = bcrypt($user['password']);
         $user = User::create($user);
 
         if ($user) {
+            $this->dispatch(new SendMailJob('admin.pages.users.mail', $user['email'], $datas));
             $data['user'] = User::find($user->id);
             $data['token'] = $user->createToken('token')->accessToken;
+            
             return $this->successResponse($data, Response::HTTP_OK);
         }
+
 
         return $this->errorResponse(config('define.register.fail'), Response::HTTP_UNAUTHORIZED);
     }
