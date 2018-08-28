bf59d8caa301edd4be0bcf4265addf0762a91292
at-namtran <nam.tran@asiantech.vn>
2018-08-28 17:10:00 +0700

    fix file js function

--- public/js/public/film.js
+++ public/js/public/film.js
@@ -51,16 +51,15 @@ $(document).ready(function () {
     }
 
     function carousel() {
-        var i;
-        var x = document.getElementsByClassName("mySlides");
-        for (i = 0; i < x.length; i++) {
-            x[i].style.display = "none"; 
+        var slides = document.getElementsByClassName("mySlides");
+        for (var i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none"; 
         }
         slideIndex++;
-        if (slideIndex > x.length) {
-            slideIndex = 1
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
         } 
-        x[slideIndex-1].style.display = "block"; 
+        slides[slideIndex-1].style.display = "block"; 
         setTimeout(carousel, 5000); 
     }
 });
