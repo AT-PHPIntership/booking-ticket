5d37f1502a246f9cbddef8bbafc01de44da69cdf
at-namtran <nam.tran@asiantech.vn>
2018-09-05 15:41:25 +0700

    fix js ticket

--- public/js/admin/add_ticket.js
+++ public/js/admin/add_ticket.js
@@ -1,27 +1,17 @@
-$(document).ready(function() {
-  $('#schedule_id').on('change', function() {
-    $("#schedule_id option:selected").each(function () {
-      var idSchedule = $(this).val();
-      $.ajax({
-        type: "GET",
-        url: "admin/tickets/film/"+idSchedule,
-        success: function( msg ) {
-          $("#film_schedule").html("<input class='form-control' type='text' value='"+ msg + "' disabled >");
-        }
-      });
-    });
-  });
-
-  $(window).on('load', function() {
-    $("#schedule_id option:selected").each(function () {
-      var idSchedule = $(this).val();
-      $.ajax({
-        type: "GET",
-        url: "admin/tickets/film/"+idSchedule,
-        success: function( msg ) {
-          $("#film_schedule").html("<input class='form-control' type='text' value='"+ msg + "' disabled >");
-        }
-      });
-    });
-  });
+$(function () {
+  chooseFilm();
+  $('#schedule_id').change(chooseFilm);
 });
+
+function chooseFilm() {
+  $("#schedule_id option:selected").each(function () {
+    var idSchedule = $(this).val();
+    $.ajax({
+      type: "GET",
+      url: "admin/tickets/film/"+idSchedule,
+      success: function( msg ) {
+        $("#film_schedule").html("<input class='form-control' type='text' value='"+ msg + "' disabled >");
+      }
+    });
+  });
+}
